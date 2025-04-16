6dbf383ebe6bbede06c9596f666e6c0274647293
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2025-04-16 10:13:34 +0530

    Update 2537.rs

--- 2501-3000/2537.rs
+++ 2501-3000/2537.rs
@@ -1,24 +1,25 @@
 use std::collections::HashMap;
 
 impl Solution {
-    pub fn count_good(nums: Vec<i32>, k: i64) -> i64 {
-        let mut l = 0;
-        let mut count = 0;
-        let mut ans = 0;
-        let mut mp = HashMap::new();
-        for r in 0..nums.len() {
-            let entry = mp.entry(nums[r]).or_insert(0);
-            count += *entry;
-            *entry += 1;
-            while l < nums.len() && (count - (mp[&nums[l]] - 1)) as i64 >= k {
-                mp.insert(nums[l], mp[&nums[l]] - 1);
-                count -= mp[&nums[l]];
-                l += 1;
-            }
-            if count as i64 >= k {
-                ans += (l + 1) as i64;
+    pub fn count_good(nums: Vec<i32>, k: i32) -> i64 {
+        let mut count = HashMap::new();
+        let mut pairs = 0i64;
+        let mut left = 0;
+        let mut res = 0;
+
+        for right in 0..nums.len() {
+            let c = count.entry(nums[right]).or_insert(0);
+            pairs += *c as i64;
+            *c += 1;
+
+            while pairs >= k as i64 {
+                res += (nums.len() - right) as i64;
+                let c = count.get_mut(&nums[left]).unwrap();
+                *c -= 1;
+                pairs -= *c as i64;
+                left += 1;
             }
         }
-        ans
+        res
     }
 }
